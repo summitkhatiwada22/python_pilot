35b53595a46313b6587ba9382abe4408a68d9680
SUMMIT KHATIWADA <summitkhatiwada@Khatiwadas-MacBook-Pro.local>
2022-07-07 21:13:42 -0500

    Uploaded basics code

---
+++ basics.py
@@ -0,0 +1,75 @@
+# touch <filename>
+# creates a file in the scurrent directory
+
+# pwd
+# shows the current working directory
+
+# echo <content>
+# displays the content
+
+# echo (content) > <filename>
+# saves the content in the filename
+
+# mv <old_filename> <new_filename>
+# rename filename
+
+# rm <filename>
+# deletes particular file
+
+# rm -r <foldername>
+# deletes the filder and all it's content
+
+
+# PRTINTING
+
+print("hello world")
+
+
+# VARIABLES
+# checkout PEP for python standards
+x = 5       # x <- 5, x is an identifier storing 5 for now
+_x = 50     # variables startingwith _ are protected
+
+name = 'Summit'     # lower case
+NAME = 'Summit'     #  upper case
+nameOne = 'Khatiwada'   # lower camel case
+NameOne = 'Khatiwada'   # upper camel case
+
+# snake case, this is a python standard
+name_one = 'Summit Khatiwada'   # use _ to separate variables
+
+
+# CONSTANTS
+# use upper case variable to make it a constant
+# its just a convention, the value can be changed, but dont change
+PI = 3.1415
+PROJECT_VERSION = '1.0.0'
+COMPANY_NAME = 'The Vedic Medic LLC'
+
+
+# DATA TYPES
+# NUMERIC DATA TYPES
+
+# integer
+x = 6
+y = -6
+
+# binary representation
+bin_1 = 0b1010
+bin_2 = 0b110011
+print(bin_1)
+print(bin_2)
+
+# octal representation
+oct_1 = 0o1574
+print(oct_1)
+
+# hexadecimal representation (hex)
+hex_1 = 0x2df2
+print(hex_1)
+
+
+
+
+
+
